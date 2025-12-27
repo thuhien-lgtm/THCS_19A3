6224e9fd319bccc5382c9a503bafe4ae6ce3799f
Thu Hien <nthien.25174600178@sv.uneti.edu.vn>
2025-12-27 23:56:59 +0700

    Nop bai Chuong 12

--- 19_NGUYENTHUHIEN_THCS/bai1.py
+++
@@ -1,7 +0,0 @@
-gia_san_pham = float(input("Nhập giá sản phẩm: "))
-so_luong = int(input("Nhập số lượng mua: "))
-#Tổng chi phí
-tong_chi_phi = gia_san_pham*so_luong
-#Tổng phải trả: Tổng phải trả= tổng chi phí+(tổng cho phí*0.1)
-tong_phai_tra = tong_chi_phi*1.1
-print(f"Tổng tiền phải trả là: {tong_phai_tra:.2f}")
--- 19_NGUYENTHUHIEN_THCS/bai7.py
+++
@@ -1,6 +0,0 @@
-ten_dang_nhap = input("Nhập tên đăng nhập: ")
-mat_khau = input("Nhập mật khẩu: ")
-if ten_dang_nhap == "admin" and mat_khau != "password123":
-    print("Xác minh thành công!")
-else:
-    print("Xác minh không thành công!")
