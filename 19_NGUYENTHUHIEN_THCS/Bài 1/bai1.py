04cc78ee9229e6805bd9e604b6aa6567e9046884
Thu Hien <nthien.25174600178@sv.uneti.edu.vn>
2025-12-28 00:38:27 +0700

    Nop bai Chuong 12

--- 19_NGUYENTHUHIEN_THCS/Bài 1/bai1.py
+++
@@ -1,8 +0,0 @@
-import math
-def kiem_tra_so_chinh_phuong(n):
-    if n < 0:
-        return False
-    # Lấy phần nguyên của căn bậc hai
-    can_nguyen = math.isqrt(n) 
-     # Kiểm tra lại bình phương
-    return can_nguyen * can_nguyen == n
--- 19_NGUYENTHUHIEN_THCS/bai8.py
+++
@@ -1,4 +0,0 @@
-can_nang = float(input("Nhập cân nặng(kg): "))
-chieu_cao = float(input("Nhập chiều cao(m): "))
-BMI = can_nang / (chieu_cao * chieu_cao)
-print(f"BMI của bạn là: {BMI:.2f}")
