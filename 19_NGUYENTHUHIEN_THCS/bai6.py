66565c54ff72986add9dc5a72df7424de503ba61
Thu Hien <nthien.25174600178@sv.uneti.edu.vn>
2025-12-28 14:43:00 +0700

    Nop bai Chuong 12

--- 19_NGUYENTHUHIEN_THCS/bai6.py
+++
@@ -1,7 +0,0 @@
-nam = int(input("Nhập năm: "))
-dieu_kien_1 = (nam % 400 == 0)
-dieu_kien_2 = (nam %4 == 0) and (nam % 100 != 0)
-if (dieu_kien_1 or dieu_kien_2):
-    print ("Năm {nam} là năm nhuận")
-else:
-    print ("Năm {nam} không là năm nhuận")
