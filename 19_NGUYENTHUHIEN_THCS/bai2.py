02accf780e2edd25a7115313bb76f4a9d361321a
Thu Hien <nthien.25174600178@sv.uneti.edu.vn>
2025-12-28 15:08:52 +0700

    Nop bai Chuong 12

--- 19_NGUYENTHUHIEN_THCS/bai2.py
+++
@@ -1,6 +0,0 @@
-tong_so_keo = int(input("Nhập tổng số kẹo: "))
-so_hoc_sinh = int(input("Nhập số học sinh: "))
-keo_nhan_duoc = tong_so_keo // so_hoc_sinh
-keo_con_thua = tong_so_keo % so_hoc_sinh
-print(f"Số kẹo mỗi học sinh nhận được: {keo_nhan_duoc}")
-print(f"Số kẹo còn thừa: {keo_con_thua}")
--- 19_NGUYENTHUHIEN_THCS/bai4.py
+++
@@ -1,4 +0,0 @@
-TY_GIA_USD = 24.500
-so_tien_vnd = float(input("Nhập số tiền VND: "))
-so_tien_usd = so_tien_vnd / TY_GIA_USD
-print(f"Số tiền USD tương ứng là: {so_tien_usd:.2f} USD")
